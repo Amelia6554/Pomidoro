77de5353b967e71500ecdf93053c188f89dce6c7
Amelia6554 <amelia.garnys@gmail.com>
2025-07-08 19:27:19 +0200

    Two channels, silent and alert

--- app/src/main/java/com/example/pomidoro/ForegroundService.java
+++ app/src/main/java/com/example/pomidoro/ForegroundService.java
@@ -17,7 +17,8 @@ import com.example.logic.TimerListener;
 
 public class ForegroundService extends Service {
     private static final int ONGOING_NOTIFICATION_ID = 101;
-    private static final String CHANNEL_ID = "1001";
+    private static final String CHANNEL_ALERT = "CHANNEL_ALERT";
+    private static final String CHANNEL_SILENT = "CHANNEL_SILENT";
     private static final String EXTRA_MINUTES = "EXTRA_MINUTES";
 
     private NotificationManager notificationManager;
@@ -27,13 +28,13 @@ public class ForegroundService extends Service {
     public void onCreate() {
         super.onCreate();
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        createServiceNotificationChannel();
+        createChannels();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         int minutes = intent.getIntExtra(EXTRA_MINUTES, 25); // domyślnie 25
-        startForeground(ONGOING_NOTIFICATION_ID, buildNotification("Pomodoro start: " + minutes + " minut"));
+        startForeground(ONGOING_NOTIFICATION_ID, buildSilentNotification("Pomodoro start: " + minutes + " minut"));
         startPomodoroTimer(minutes);
         return START_STICKY;
     }
@@ -48,25 +49,44 @@ public class ForegroundService extends Service {
 
             @Override
             public void onFinish() {
-                updateNotification("Pomodoro zakończone!");
+                notificationManager.notify(314, buildAlertNotification("Pomodoro zakończone!"));
                 stopSelf(); // opcjonalnie zatrzymaj serwis po zakończeniu
             }
         });
-        timer.start_timer(minutes * 60); // np. 25 minut
+        int totalSeconds = minutes * 60;
+        updateNotification("Pozostało: " + totalSeconds + " sek.");
+        timer.start_timer(totalSeconds); // np. 25 minut
     }
 
     private void updateNotification(String contentText) {
-        Notification notification = buildNotification(contentText);
+        Notification notification = buildSilentNotification(contentText);
         notificationManager.notify(ONGOING_NOTIFICATION_ID, notification);
     }
 
-    private Notification buildNotification(String contentText) {
+
+//    private Notification buildNotification(String channelId, String title, String contentText, boolean ongoing) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(
+//                this, 0, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+//        );
+//
+//        return new NotificationCompat.Builder(this, channelId)
+//                .setContentTitle(title)
+//                .setContentText(contentText)
+//                .setSmallIcon(R.drawable.ic_baseline_access_alarm_24)
+//                .setContentIntent(pendingIntent)
+//                .setOngoing(ongoing)
+//                .build();
+//    }
+
+
+    private Notification buildSilentNotification(String contentText) {
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(
                 this, 0, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
         );
 
-        return new NotificationCompat.Builder(this, CHANNEL_ID)
+        return new NotificationCompat.Builder(this, CHANNEL_SILENT)
                 .setContentTitle("Pomodoro")
                 .setContentText(contentText)
                 .setSmallIcon(R.drawable.ic_baseline_access_alarm_24)
@@ -75,17 +95,60 @@ public class ForegroundService extends Service {
                 .build();
     }
 
-    private void createServiceNotificationChannel() {
+
+    private Notification buildAlertNotification(String contentText) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        return new NotificationCompat.Builder(this, CHANNEL_ALERT)
+                .setContentTitle("Pomodoro")
+                .setContentText(contentText)
+                .setSmallIcon(R.drawable.ic_baseline_alarm_on_24)
+                .setContentIntent(pendingIntent)
+                .setOngoing(false)
+                .build();
+    }
+
+
+
+    private void createChannels() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Pomodoro channel",
+            NotificationManager nm = getSystemService(NotificationManager.class);
+
+            // Kanał cichy
+            NotificationChannel silentChannel = new NotificationChannel(
+                    CHANNEL_SILENT,
+                    "Ciche powiadomienia",
                     NotificationManager.IMPORTANCE_LOW
             );
-            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-            notificationManager.createNotificationChannel(channel);
+            silentChannel.setSound(null, null);
+            silentChannel.enableVibration(false);
+
+            // Kanał głośny
+            NotificationChannel alertChannel = new NotificationChannel(
+                    CHANNEL_ALERT,
+                    "Ważne powiadomienia",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+            nm.createNotificationChannel(silentChannel);
+            nm.createNotificationChannel(alertChannel);
         }
     }
+
+//    private void createServiceNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel channel = new NotificationChannel(
+//                    CHANNEL_ID,
+//                    "Pomodoro channel",
+//                    NotificationManager.IMPORTANCE_LOW
+//            );
+//            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
 
     // Static methods to start/stop service
     public static void startService(Context context, int minutes) {
